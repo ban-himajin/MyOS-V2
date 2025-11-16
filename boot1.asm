abe02d1cf0bf662ad4b94181fe1b7e2a73198778
バンされた暇人 <129594814+ban-himajin@users.noreply.github.com>
2025-11-16 17:32:58 +0900

    CHSからLBAへの変更

--- boot1.asm
+++ boot1.asm
@@ -2,10 +2,24 @@
 [org 0x7c00]
 
 ;----------定数作成スペース---------
-%define SECONDSIZE 72
+
+;1ならtrue,0ならfalse
+%define LBA 0
+
+;LBAの設定
+%define LBA_SIZE 1
+%define LBA_SECTOR_OFFSET 0x0600
+%define LBA_SECTOR_SEGMENT 0x0000
+%define LBA_START_SECTOR 1
+
+;セカンドローダーの設定
+;%define SECOND_SECTOR 72
+%define SECOND_SIZE 88
+%define SECOND_SECTOR_OFFSET 0x8000
+%define SECOND_SECTOR_SEGMENT 0x0000
+%define SECOND_START_SECTOR 1
 
 %define VGA 0xb8000
-
 
 ;----------------------------------
 
@@ -17,8 +31,8 @@ start_msg:
     db "BanHimaBootLoader Ver:a 0.0.1", 10, 0
 
 error_msg:
-    db "|SecondBoot no found|", 0
-
+    db "|Select sector no found|", 0
+    
 back_color_32bit:
     db 0x00
 
@@ -31,15 +45,41 @@ corsor_Y:
 corsor_X:
     db 0x00
 
+Boot_Drive:
+    db 0
+    
+sector_size:
+    dw 0
+
+sector_offset:
+    dw 0
+
+sector_segment:
+    dw 0
+
+sector_num:
+    dq 0
+
+DAP:
+    db 0x10;DAPの全体バイト数
+    db 0
+    dw 0;読み込むセクタ数
+    dw 0;オフセット
+    dw 0;セグメント
+    dq 0;読み込みセクタ開始場所(セクタ0からの計算ではなく現在位置からの移動数)
+
+
 ;section .bss
 
 ;section .text
 
 
 no_displey_corsor:
+    pusha
     mov dx, 0x3D4
     mov al, 0x0A
     out dx, al
+
     inc dx
     mov al, 0x20
     out dx, al
@@ -49,6 +89,7 @@ no_displey_corsor:
     mov dh, 0
     mov dl, 0
     int 0x10
+    popa
     ret
 
 load_corsor:
@@ -97,7 +138,6 @@ print:;printの初めの部分
 .line_break:
     call load_corsor
     jmp .print_next
-
 .done_print:
     mov byte [color], 0x07
     call load_corsor
@@ -123,9 +163,28 @@ get_corsor:
     mov byte [corsor_Y], dh
     mov byte [corsor_X], dl
     ret
+DAPset:
+    push ax
 
+    mov ax, [sector_size]
+    mov word[DAP + 2], ax
+
+    mov ax, [sector_offset]
+    mov word[DAP + 4], ax
+
+    mov ax, [sector_segment]
+    mov word[DAP + 6], ax
+
+    mov ax, word[sector_num]
+    mov [DAP + 8], ax
+    mov ax, word[sector_num + 2]
+    mov [DAP + 10], ax
+
+    pop ax
+    ret
 start:
     cli
+    mov byte[Boot_Drive], dl
     xor ax, ax
     mov ds, ax
     mov es, ax
@@ -135,9 +194,13 @@ start:
     ;スタックの開始位置
     sti
     call no_displey_corsor
+    call load_corsor
     call clean_screen
     call start_print
-    jmp load_second
+    call load_disk
+    call load_sector
+    ;jmp 0x0000:0x8000
+    jmp SECOND_SECTOR_SEGMENT:SECOND_SECTOR_OFFSET
 
 start_print:;ブートローダー実行時に実行
     mov si, start_msg
@@ -165,23 +228,62 @@ error_print_vga:
     call print_vga
     jmp $
 
-load_second:
-    mov bx, 0x8000
-    xor ax, ax
-    mov es, ax
+load_disk:
+    mov ax, LBA
+    cmp ax, 1
+    je load_lba
+    jmp load_second
 
-    ;ファイルの読み込み
-    mov ah, 0x02
-    mov al, SECONDSIZE
-    mov ch, 0
-    mov cl, 2
-    ;ロード先のセクタ番号
-    mov dh, 0
-    mov dl, 0x80
+load_lba:
+    push ax
+
+    mov ax, LBA_SIZE
+    mov [sector_size], ax
+
+    mov ax, LBA_SECTOR_OFFSET
+    mov [sector_offset], ax
+
+    mov ax, LBA_SECTOR_SEGMENT
+    mov [sector_segment], ax
+
+    mov ax, LBA_START_SECTOR
+    mov [sector_num], ax
+
+    pop ax
+    call DAPset
+    call load_sector
+    jmp load_second
+
+load_second:
+    push ax
+
+    mov ax, SECOND_SIZE
+    mov [sector_size], ax
+
+    mov ax, SECOND_SECTOR_OFFSET
+    mov [sector_offset], ax
+
+    mov ax, SECOND_SECTOR_SEGMENT
+    mov [sector_segment], ax
+
+    mov ax, SECOND_START_SECTOR
+    mov [sector_num], ax
+
+    pop ax
+    call DAPset
+    call load_sector
+    ret
+
+load_sector:
+
+    mov si, DAP
+    mov ah, 0x42
+    mov dl, [Boot_Drive]
     int 0x13
+    
     jc error_print
 
-    jmp 0x0000:0x8000
+    ret
     ;jmp 0x0000:0xFFFFF
 
 times 510-($-$$) db 0
--- boot2.asm
+++ boot2.asm
@@ -1,18 +1,16 @@
 ;----------定数作成スペース---------
-%define KERNEL_LOAD_SEGMENT 0x1000
-;セグメント
-%define KERNEL_LOAD_OFFSET 0x0000
-;オフセット
-;LoadPlace1 * 16 + LoadPlace2が配置場所
 
-;%define SECONDBOOTSIZE
 
 %define KERNEL_SIZE 1
+%define KERNEL_SECTOR_SEGMENT 0x1000
+;セグメント
+%define KERNEL_SECTOR_OFFSET 0x0000
+;オフセット
+;セグメント * 16 + オフセットが配置場所
+%define KERNEL_START_SECTOR 1
 
-%define KERNEL_COPY_PLACE 0x100000
+;%define KERNEL_COPY_PLACE 0x100000
 ;カーネルのコピー先指定
-
-;%define VGA 0xb8000
 
 ;----------------------------------
 section .data;bit共有データセクション
@@ -41,10 +39,31 @@ color_red:
 color_blue:
     db 0x01
 
+Boot_Drive:
+    db 0
+
+sector_size:
+    dw 0
+
+sector_offset:
+    dw 0
+
+sector_segment:
+    dw 0
+
+sector_num:
+    dq 0
+
+DAP:
+    db 0x10;DAPの全体バイト数
+    db 0
+    dw 0;読み込むセクタ数
+    dw 0;オフセット
+    dw 0;セグメント
+    dq 0;読み込みセクタ開始場所(セクタ0からの計算ではなく現在位置からの移動数)
 
 [BITS 16]
 [org 0x8000]
-;[org 0x100000]
 
 section .data;16bit専用データセクション
 
@@ -52,10 +71,11 @@ Second_Start_msg:
     db "Start Second Boot", 10, 0
 
 result_load_msg:
-    db "kernel found", 10, 0
+    ;db "kernel found", 10, 0
+    db "Select sector found", 10, 0
 
 load_error:
-    db "|Kernel no found|", 10, 0
+    db "|Select sector no found|", 10, 0
 
 ;32bitGDTの設定
 gdt32bit_start:
@@ -88,7 +108,7 @@ gdt32bit_descriptor:
 ;section .bss
 
 section .text
-
+;jmp $
 jmp start
 
 print:;print関数
@@ -97,7 +117,6 @@ print:;print関数
     je .done_print
     cmp al, 10
     je .line_break_16bit
-    jne .print_next
 .print_next:
     mov ah, 0x0e
     mov bh, 0x00
@@ -133,8 +152,31 @@ load_corsor_16bit:;カーソル位置を得る関数
     int 0x10
     ret
 
+DAPset:
+    push ax
+
+    mov ax, [sector_size]
+    mov word[DAP + 2], ax
+
+    mov ax, [sector_offset]
+    mov word[DAP + 4], ax
+
+    mov ax, [sector_segment]
+    mov word[DAP + 6], ax
+
+    mov ax, word[sector_num]
+    mov [DAP + 8], ax
+    mov ax, word[sector_num + 2]
+    mov [DAP + 10], ax
+
+    pop ax
+    ret
+
 start:;初めの部分
     cli
+
+    mov byte[Boot_Drive], dl
+
     xor ax, ax
     ;xor ax, 0x1000
     mov ds, ax
@@ -144,13 +186,14 @@ start:;初めの部分
     mov sp, 0x9000
     ;mov sp, 0xFFF0
     ;スタックの開始位置
+
     sti
 
     call load_corsor_16bit
     call second_start_print
     call enable_a20_fast
     ;jmp $
-    jmp load_kernel
+    call load_disk
     jmp setup_protect_mode
 
 second_start_print:
@@ -164,23 +207,44 @@ enable_a20_fast:;A20を有効にする関数
     out 0x92,al
     ret
 
-load_kernel:;次に使うファイルをロードする
-    mov ax, KERNEL_LOAD_SEGMENT
-    mov es, ax
-    mov bx, KERNEL_LOAD_OFFSET
-    ;配置場所指定
-    mov ah, 0x02
-    ;読み取り開始セクタ数
-    mov al, KERNEL_SIZE
-    ;カーネルのサイズ数
-    mov ch, 0
-    mov cl, 2
-    mov dl, 0x80
-    int 0x13
-    ;jc load_kernel_error
-    jmp result_load_print
+load_disk:
+    jmp load_kernel
 
-load_kernel_error:;ロード失敗
+
+load_kernel:;次に使うファイルをロードする
+    push ax
+
+    mov ax, KERNEL_SIZE
+    mov [sector_size], ax
+
+    mov ax, KERNEL_SECTOR_OFFSET
+    mov [sector_offset], ax
+
+    mov ax, KERNEL_SECTOR_SEGMENT
+    mov [sector_segment], ax
+
+    mov ax, KERNEL_START_SECTOR
+    mov [sector_num], ax
+
+    pop ax
+    call DAPset
+    call load_sector
+
+    ret
+
+
+load_sector:
+
+    mov si, DAP
+    mov ah, 0x42
+    mov dl, [Boot_Drive]
+    int 0x13
+    
+    jc load_sector_error
+    call result_load_print
+    ret
+
+load_sector_error:;ロード失敗
     mov si, load_error
     mov dh, [color_red]
     mov byte[color], dh
@@ -190,7 +254,7 @@ load_kernel_error:;ロード失敗
 result_load_print:;ロード成功
     mov si, result_load_msg
     call print
-    jmp setup_protect_mode
+    ret
 
 setup_protect_mode:;プロテクトモードへ移行
     call load_corsor_16bit
@@ -213,13 +277,13 @@ setup_protect_mode:;プロテクトモードへ移行
     ;%2:ハンドラアドレス
     ;%3:セグメントセレクタ
 
-    mov word [idt + %1 * 8 + 0], (%2-$$) & 0xffff   ;offset_low
-    ;mov word [idt + %1 * 8 + 0], %2-$$ & 0xffff    ;offset_low
-    mov word [idt + %1 * 8 + 2], %3                 ;selector
-    mov byte [idt + %1 * 8 + 4], 0                  ;zero
-    mov byte [idt + %1 * 8 + 5], 0x8e               ;type_attr(割り込みゲート)
-    mov word [idt + %1 * 8 + 6], (%2-$$) >> 16      ;offset_high
-    ;mov word [idt + %1 * 8 + 6], %2 >> 16       ;offset_high
+    mov word [idt_32bit + %1 * 8 + 0], (%2-$$) & 0xffff   ;offset_low
+    ;mov word [idt_32bit + %1 * 8 + 0], %2-$$ & 0xffff    ;offset_low
+    mov word [idt_32bit + %1 * 8 + 2], %3                 ;selector
+    mov byte [idt_32bit + %1 * 8 + 4], 0                  ;zero
+    mov byte [idt_32bit + %1 * 8 + 5], 0x8e               ;type_attr(割り込みゲート)
+    mov word [idt_32bit + %1 * 8 + 6], (%2-$$) >> 16      ;offset_high
+    ;mov word [idt_32bit + %1 * 8 + 6], %2 >> 16       ;offset_high
 %endmacro
 
 ;------------------------------------------
@@ -264,15 +328,15 @@ gdt64bit_descriptor:
     dd gdt64bit_start
 
 ;IDTの作成
-idt:
+idt_32bit:
     ;times 256 * 6 db 0
     times 256 * 8 db 0
-idt_ptr:
+idt_ptr_32bit:
     dw 256 * 8 - 1
-    dd idt
+    dd idt_32bit
 
 ;ページングテーブルの定義
-%define PAGE_FLAGS 0x003  ; Present + Read/Write
+%define PAGE_FLAGS 0x03  ; Present + Read/Write
 
 align 4096
 pml4_table_64bit:
@@ -440,7 +504,6 @@ start_32bit:;32bit開始位置
     call start_setup_32bit
     call start_print_32bit
     lgdt[gdt64bit_descriptor]
-    ;jmp $
     call paging_32bit
     jmp 0x08:start_64bit
 
@@ -488,282 +551,308 @@ idt_setup_32bit:
     set_idt_entry_32bit 21, r21_29_32bit, 0x08
     set_idt_entry_32bit 30, sx_32bit, 0x08
     set_idt_entry_32bit 31, r31_32bit, 0x08
-    lidt [idt_ptr]
+    lidt [idt_ptr_32bit]
     ret
 
 ;ここから下が例外ハンドラの処理をする関数
-isr0_msg_32bit:;0
-    db "de Ecode=0",32 ,0
 isr0_32bit:
     cli
     pushad
+    mov esi, .isr0_msg_32bit
     call error_color
-    mov esi, isr0_msg_32bit
     call print_32bit
     popad
     hlt
     ;iret
-
-db_msg_32bit:;1
-    db "db Ecode=1",32 ,0
+.isr0_msg_32bit:;0
+    db "de Ecode=0",32 ,0
 db_32bit:
     cli
     pushad
+    mov esi, .db_msg_32bit
     call error_color
-    mov si,db_msg_32bit
+    call print_32bit
     popad
     hlt
     ;iret
-
-nmi_msg_32bit:;2
-    db "nmi Ecode=2",32 ,0
+.db_msg_32bit:;1
+    db "db Ecode=1",32 ,0
 nmi_32bit:
     cli
     pushad
+    mov esi, .nmi_msg_32bit
     call error_color
-    mov si,nmi_msg_32bit
+    call print_32bit
     popad
     hlt
     ;iret
-
-bp_msg_32bit:;3
-    db "bp Ecode=3",32 ,0
+.nmi_msg_32bit:;2
+    db "nmi Ecode=2",32 ,0
 bp_32bit:
     cli
     pushad
+    mov esi, .bp_msg_32bit
     call error_color
-    mov si,bp_msg_32bit
+    call print_32bit
     popad
     hlt
     ;iret
-
-of_msg_32bit:;4
-    db "of Ecode=4",32 ,0
+.bp_msg_32bit:;3
+    db "bp Ecode=3",32 ,0
 of_32bit:
     cli
     pushad
+    mov esi, .of_msg_32bit
     call error_color
-    mov si,of_msg_32bit
+    call print_32bit
     popad
     hlt
     ;iret
-
-br_msg_32bit:;5
-    db "br Ecode=5",32 ,0
+.of_msg_32bit:;4
+    db "of Ecode=4",32 ,0
 br_32bit:
     cli
     pushad
+    mov esi, .br_msg_32bit
     call error_color
-    mov si,br_msg_32bit
+    call print_32bit
     popad
     hlt
     ;iret
-
-ud_msg_32bit:;6
-    db "ud Ecode=6",32 ,0
+.br_msg_32bit:;5
+    db "br Ecode=5",32 ,0
 ud_32bit:
     cli
     pushad
+    mov esi, .ud_msg_32bit
     call error_color
-    mov esi, ud_msg_32bit
+    call print_32bit
     popad
     hlt
     ;iret
-
-nm_msg_32bit:;7
-    db "nm Ecode=7",32 ,0
+.ud_msg_32bit:;6
+    db "ud Ecode=6",32 ,0
 nm_32bit:
     cli
     pushad
+    mov esi, .nm_msg_32bit
     call error_color
-    mov si,nm_msg_32bit
+    call print_32bit
     popad
     hlt
     ;iret
-
-df_msg_32bit:;8
-    db "df Ecode=8",32 ,0
+.nm_msg_32bit:;7
+    db "nm Ecode=7",32 ,0
 df_32bit:
     cli
     pushad
+    mov esi, .df_msg_32bit
     call error_color
-    mov si,df_msg_32bit
+    call print_32bit
     popad
     hlt
     ;iret
-
-r9_msg_32bit:;9
-    db "r9(-) Ecode=9",32 ,0
+.df_msg_32bit:;8
+    db "df Ecode=8",32 ,0
 r9_32bit:
     cli
     pushad
+    mov esi, .r9_msg_32bit
     call error_color
-    mov si,r9_msg_32bit
+    call print_32bit
     popad
     hlt
     ;iret
-
-ts_msg_32bit:;10
-    db "ts Ecode=10",32 ,0
+.r9_msg_32bit:;9
+    db "r9(-) Ecode=9",32 ,0
 ts_32bit:
     cli
     pushad
+    mov esi, .ts_msg_32bit
     call error_color
-    mov si,ts_msg_32bit
+    call print_32bit
     popad
     hlt
     ;iret
-
-np_msg_32bit:;11
-    db "np Ecode=11",32 ,0
+.ts_msg_32bit:;10
+    db "ts Ecode=10",32 ,0
 np_32bit:
     cli
     pushad
+    mov esi, .np_msg_32bit
     call error_color
-    mov si,np_msg_32bit
+    call print_32bit
     popad
     hlt
     ;iret
-
-ss_msg_32bit:;12
-    db "ss Ecode=12",32 ,0
+.np_msg_32bit:;11
+    db "np Ecode=11",32 ,0
 ss_32bit:
     cli
     pushad
+    mov esi, .ss_msg_32bit
     call error_color
-    mov si,ss_msg_32bit
+    call print_32bit
     popad
     hlt
     ;iret
-
-gp_msg_32bit:;13
-    db "gp Ecode=13",32 ,0
+.ss_msg_32bit:;12
+    db "ss Ecode=12",32 ,0
 gp_32bit:
     cli
     pushad
+    mov esi, .gp_msg_32bit
     call error_color
-    mov si,gp_msg_32bit
+    call print_32bit
     popad
     hlt
     ;iret
-
-pf_msg_32bit:;14
-    db "pf Ecode=14",32 ,0
+.gp_msg_32bit:;13
+    db "gp Ecode=13",32 ,0
 pf_32bit:;
     cli
     pushad
+    mov esi, .pf_msg_32bit
     call error_color
-    mov esi, pf_msg_32bit
+    call print_32bit
     popad
     hlt
     ;iret
+.pf_msg_32bit:;14
+    db "pf Ecode=14",32 ,0
 
-r15_msg_32bit:;15
-    db "r15(-) Ecode=15",32 ,0
 r15_32bit:
     cli
     pushad
+    mov esi, .r15_msg_32bit
     call error_color
-    mov si,r15_msg_32bit
+    call print_32bit
     popad
     hlt
     ;iret
-
-mf_msg_32bit:;16
-    db "mf Ecode=16",32 ,0
+.r15_msg_32bit:;15
+    db "r15(-) Ecode=15",32 ,0
 mf_32bit:
     cli
     pushad
+    mov esi, .mf_msg_32bit
     call error_color
-    mov si,mf_msg_32bit
+    call print_32bit
     popad
     hlt
     ;iret
-
-ac_msg_32bit:;17
-    db "ac Ecode=17",32 ,0
+.mf_msg_32bit:;16
+    db "mf Ecode=16",32 ,0
 ac_32bit:
     cli
     pushad
+    mov esi, .ac_msg_32bit
     call error_color
-    mov si,ac_msg_32bit
+    call print_32bit
     popad
     hlt
     ;iret
-
-mc_msg_32bit:;18
-    db "mc Ecode=18",32 ,0
+.ac_msg_32bit:;17
+    db "ac Ecode=17",32 ,0
 mc_32bit:
     cli
     pushad
+    mov esi, .mc_msg_32bit
     call error_color
-    mov si,mc_msg_32bit
+    call print_32bit
     popad
     hlt
     ;iret
-
-xf_msg_32bit:;19
-    db "xf Ecode=19",32 ,0
+.mc_msg_32bit:;18
+    db "mc Ecode=18",32 ,0
 xf_32bit:
     cli
     pushad
+    mov esi, .xf_msg_32bit
     call error_color
-    mov si,xf_msg_32bit
+    call print_32bit
     popad
     hlt
     ;iret
-
-ve_msg_32bit:;20
-    db "ve Ecode=20",32 ,0
+.xf_msg_32bit:;19
+    db "xf Ecode=19",32 ,0
 ve_32bit:
     cli
     pushad
+    mov esi, .ve_msg_32bit
     call error_color
-    mov si,ve_msg_32bit
+    call print_32bit
     popad
     hlt
     ;iret
-
-r21_29_msg_32bit:;21-29
-    db "r21-29(-)br Ecode=21-29",32 ,0
+.ve_msg_32bit:;20
+    db "ve Ecode=20",32 ,0
 r21_29_32bit:
     cli
     pushad
+    mov esi, .r21_29_msg_32bit
     call error_color
-    mov si,r21_29_msg_32bit
+    call print_32bit
     popad
     hlt
     ;iret
-
-sx_msg_32bit:;30
-    db "sx Ecode=30",32 ,0
+.r21_29_msg_32bit:;21-29
+    db "r21-29(-)br Ecode=21-29",32 ,0
 sx_32bit:
     cli
     pushad
+    mov esi, .sx_msg_32bit
     call error_color
-    mov si,sx_msg_32bit
+    call print_32bit
     popad
     hlt
     ;iret
-
-r31_msg_32bit:;31
-    db "r31(-) Ecode=31",32 ,0
+.sx_msg_32bit:;30
+    db "sx Ecode=30",32 ,0
 r31_32bit:
     cli
     pushad
+    mov esi, .r31_msg_32bit
     call error_color
-    mov si,r31_msg_32bit
+    call print_32bit
     popad
     hlt
     ;iret
+.r31_msg_32bit:;31
+    db "r31(-) Ecode=31",32 ,0
 ;---------------------------------
 
 [bits 64]
+;-----------64bitマクロ定義セクション-------------
+%macro set_idt_entry_64bit 3
+    ; %1: 割り込み番号
+    ; %2: ハンドララベル
+    ; %3: セグメントセレクタ
+
+    mov word [idt_64bit + %1 * 16 + 0], (%2-$$) & 0xffff              ; offset_low
+    mov word [idt_64bit + %1 * 16 + 2], (%3-$$)                       ; selector
+    mov byte [idt_64bit + %1 * 16 + 4], 0                             ; IST
+    mov byte [idt_64bit + %1 * 16 + 5], 0x8e                          ; type_attr
+    ;mov word [idt_64bit + %1 * 16 + 6], (%2 >> 16) & 0xffff          ; offset_mid
+    mov word [idt_64bit + %1 * 16 + 6], (%2-$$ >> 16) & 0xffff        ; offset_mid
+    ;mov dword [idt_64bit + %1 * 16 + 8], (%2 >> 32) & 0xffffffff     ; offset_high
+    mov dword [idt_64bit + %1 * 16 + 8], (%2-$$ >> 32) & 0xffffffff   ; offset_high
+    mov dword [idt_64bit + %1 * 16 + 12], 0                           ; zero
+%endmacro
+
+;------------------------------------------------
+
 ;section .data
 
 start_msg_64bit:
     db "Start 64bit Mode!", 10, 0
-    ;db "SSSSSSSSSS", 10, 0
+
+idt_64bit:
+    times 256 * 16 db 0           ; 256エントリ × 16バイト = 4096バイト
+
+idt_ptr_64bit:
+    dw 256 * 16 - 1               ; limit（サイズ - 1）
+    dq idt_64bit                  ; base（IDTのアドレス）
 
 ;section .dss
 
@@ -879,8 +968,257 @@ start_64bit:;64bit開始場所
     ;jmp $
     ;call clean_screen_64bit
     ;cli
+    call idt_setup_64bit
     call start_setup_64bit
     call start_print_64bit
     ;jmp $
     ;sti
     jmp $
+
+idt_setup_64bit:
+    set_idt_entry_64bit 0, isr0_64bit, 0x08
+    set_idt_entry_64bit 1, db_64bit, 0x08
+    set_idt_entry_64bit 2, nmi_64bit, 0x08
+    set_idt_entry_64bit 3, db_64bit, 0x08
+;    set_idt_entry_64bit 4, of_64bit, 0x08
+;    set_idt_entry_64bit 5, br_64bit, 0x08
+    set_idt_entry_64bit 6, ud_64bit, 0x08
+    set_idt_entry_64bit 7, nm_64bit, 0x08
+    set_idt_entry_64bit 8, df_64bit, 0x08
+;    set_idt_entry_64bit 9, r9_64bit, 0x08
+;    set_idt_entry_64bit 10, ts_64bit, 0x08
+;    set_idt_entry_64bit 11, np_64bit, 0x08
+;    set_idt_entry_64bit 12, ss_64bit, 0x08
+;    set_idt_entry_64bit 13, gp_64bit, 0x08
+;    set_idt_entry_64bit 14, pf_64bit, 0x08
+;    set_idt_entry_64bit 15, r15_64bit, 0x08
+;    set_idt_entry_64bit 16, mf_64bit, 0x08
+;    set_idt_entry_64bit 17, ac_64bit, 0x08
+;    set_idt_entry_64bit 18, mc_64bit, 0x08
+;    set_idt_entry_64bit 19, xf_64bit, 0x08
+;    set_idt_entry_64bit 20, ve_64bit, 0x08
+;    set_idt_entry_64bit 21, r21_29_64bit, 0x08
+;    set_idt_entry_64bit 30, sx_64bit, 0x08
+;    set_idt_entry_64bit 31, r31_64bit, 0x08
+    lidt [idt_ptr_64bit]
+    ret
+
+;ここから下が例外ハンドラの処理をする関数
+isr0_64bit:
+    cli
+    mov rsi, .isr0_msg_64bit
+    call error_color
+    call print_64bit
+    hlt
+    ;iret
+.isr0_msg_64bit:;0
+    db "de Ecode=0",64 ,0
+db_64bit:
+    cli
+    mov rsi, .db_msg_64bit
+    call error_color
+    call print_64bit
+    hlt
+    ;iret
+.db_msg_64bit:;1
+    db "db Ecode=1",64 ,0
+nmi_64bit:
+    cli
+    mov rsi, .nmi_msg_64bit
+    call error_color
+    call print_64bit
+    hlt
+    ;iret
+.nmi_msg_64bit:;2
+    db "nmi Ecode=2",64 ,0
+bp_64bit:
+    cli
+    mov rsi, .bp_msg_64bit
+    call error_color
+    call print_64bit
+    hlt
+    ;iret
+.bp_msg_64bit:;3
+    db "bp Ecode=3",64 ,0
+of_64bit:
+    cli
+    mov rsi, .of_msg_64bit
+    call error_color
+    call print_64bit
+    hlt
+    ;iret
+.of_msg_64bit:;4
+    db "of Ecode=4",64 ,0
+br_64bit:
+    cli
+    mov rsi, .br_msg_64bit
+    call error_color
+    call print_64bit 
+    hlt
+    ;iret
+.br_msg_64bit:;5
+    db "br Ecode=5",64 ,0
+ud_64bit:
+    cli
+    mov rsi, .ud_msg_64bit
+    call error_color
+    call print_64bit
+    hlt
+    ;iret
+.ud_msg_64bit:;6
+    db "ud Ecode=6",64 ,0
+nm_64bit:
+    cli
+    mov rsi, .nm_msg_64bit
+    call error_color
+    call print_64bit
+    hlt
+    ;iret
+.nm_msg_64bit:;7
+    db "nm Ecode=7",64 ,0
+df_64bit:
+    cli
+    mov rsi, .df_msg_64bit
+    call error_color
+    call print_64bit
+    hlt
+    ;iret
+.df_msg_64bit:;8
+    db "df Ecode=8",64 ,0
+r9_64bit:
+    cli
+    mov rsi, .r9_msg_64bit
+    call error_color
+    call print_64bit
+    hlt
+    ;iret
+.r9_msg_64bit:;9
+    db "r9(-) Ecode=9",64 ,0
+ts_64bit:
+    cli
+    mov rsi, .ts_msg_64bit
+    call error_color
+    call print_64bit
+    hlt
+    ;iret
+.ts_msg_64bit:;10
+    db "ts Ecode=10",64 ,0
+np_64bit:
+    cli
+    mov rsi, .np_msg_64bit
+    call error_color
+    call print_64bit
+    hlt
+    ;iret
+.np_msg_64bit:;11
+    db "np Ecode=11",64 ,0
+ss_64bit:
+    cli
+    mov rsi, .ss_msg_64bit
+    call error_color
+    call print_64bit
+    hlt
+    ;iret
+.ss_msg_64bit:;12
+    db "ss Ecode=12",64 ,0
+gp_64bit:
+    cli
+    mov rsi, .gp_msg_64bit
+    call error_color
+    call print_64bit
+    hlt
+    ;iret
+.gp_msg_64bit:;13
+    db "gp Ecode=13",64 ,0
+pf_64bit:;
+    cli
+    mov rsi, .pf_msg_64bit
+    call error_color
+    call print_64bit 
+    hlt
+    ;iret
+.pf_msg_64bit:;14
+    db "pf Ecode=14",64 ,0
+
+r15_64bit:
+    cli
+    mov rsi, .r15_msg_64bit
+    call error_color
+    call print_64bit
+    hlt
+    ;iret
+.r15_msg_64bit:;15
+    db "r15(-) Ecode=15",64 ,0
+mf_64bit:
+    cli
+    mov rsi, .mf_msg_64bit
+    call error_color
+    call print_64bit 
+    hlt
+    ;iret
+.mf_msg_64bit:;16
+    db "mf Ecode=16",64 ,0
+ac_64bit:
+    cli
+    mov rsi, .ac_msg_64bit
+    call error_color
+    call print_64bit
+    hlt
+    ;iret
+.ac_msg_64bit:;17
+    db "ac Ecode=17",64 ,0
+mc_64bit:
+    cli
+    mov rsi, .mc_msg_64bit
+    call error_color
+    call print_64bit
+    hlt
+    ;iret
+.mc_msg_64bit:;18
+    db "mc Ecode=18",64 ,0
+xf_64bit:
+    cli
+    mov rsi, .xf_msg_64bit
+    call error_color
+    call print_64bit
+    hlt
+    ;iret
+.xf_msg_64bit:;19
+    db "xf Ecode=19",64 ,0
+ve_64bit:
+    cli
+    mov rsi, .ve_msg_64bit
+    call error_color
+    call print_64bit
+    hlt
+    ;iret
+.ve_msg_64bit:;20
+    db "ve Ecode=20",64 ,0
+r21_29_64bit:
+    cli
+    mov rsi, .r21_29_msg_64bit
+    call error_color
+    call print_64bit
+    hlt
+    ;iret
+.r21_29_msg_64bit:;21-29
+    db "r21-29(-)br Ecode=21-29",64 ,0
+sx_64bit:
+    cli
+    mov rsi, .sx_msg_64bit
+    call error_color
+    call print_64bit
+    hlt
+    ;iret
+.sx_msg_64bit:;30
+    db "sx Ecode=30",64 ,0
+r31_64bit:
+    cli
+    mov rsi, .r31_msg_64bit
+    call error_color
+    call print_64bit
+    hlt
+    ;iret
+.r31_msg_64bit:;31
+    db "r31(-) Ecode=31",64 ,0
+;---------------------------------
