37002d6efec7bbf3e8d31235824459c620b9d5c4
バンされた暇人 <129594814+ban-himajin@users.noreply.github.com>
2026-02-09 18:42:28 +0900

    add std librarys
    New forder

---
+++ Boot_files/Library/std/num_type/num_type.h
@@ -0,0 +1,15 @@
+#ifndef NUM_TYPE
+#define NUM_TYPE 1
+#define NMU_TYPE_VER 1.0
+
+typedef unsigned long long uint64;
+typedef unsigned int uint32;
+typedef unsigned short uint16;
+typedef unsigned char uint8;
+
+typedef long long int64;
+typedef int int32;
+typedef short int16;
+typedef char int8;
+
+#endif
