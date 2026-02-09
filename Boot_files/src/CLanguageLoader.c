b93ce7c4504f3174912f2bdecbaf38c384080d29
バンされた暇人 <129594814+ban-himajin@users.noreply.github.com>
2026-02-09 18:44:35 +0900

    updata CLoder
    std library use updata

--- Boot_files/src/CLanguageLoader.c
+++ Boot_files/src/CLanguageLoader.c
@@ -1,22 +1,23 @@
 #include "../Library/VGA_Driver/VGA_Driver_v1.h"
+#include "../Library/std\num_type/num_type.h"
 
 
 void __attribute__((section(".Ctext"))) C_loader_main(void){
-    unsigned char VGA_x = 0;
-    unsigned char VGA_y = 0;
-    unsigned short *VGA = get_vga_memory();
-    clean_screen(' ', COLLAR(Black,Black));
-    write_vga_text(&VGA, 'A', COLLAR(Black, White));
-    write_vga_texts(&VGA,"hello world", COLLAR(Black, White));
+    uint8 VGA_x = 0;
+    uint8 VGA_y = 0;
+    uint16 *VGA = get_vga_memory();
+    clean_screen(' ', COLOR(Black,Black));
+    write_vga_text(&VGA, 'A', COLOR(Black, White));
+    write_vga_texts(&VGA,"hello world", COLOR(Black, White));
     while(1){
-        //write_vga_texts(&VGA,"tset", COLLAR(Black, White));
+        //write_vga_texts(&VGA,"tset", COLOR(Black, White));
     };
     return ;
 }
 
 
 void __attribute__((section(".Ctext"))) isr_C_function(void){
-    clean_screen(' ', COLLAR(Red,Black));
+    clean_screen('E', COLOR(Red,Black));
     while(1){};
     return ;
 }
