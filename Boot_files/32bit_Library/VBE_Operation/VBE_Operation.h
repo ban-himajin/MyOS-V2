3c04cf87ce29411cb8608c39b292376fb0b48c60
バンされた暇人 <129594814+ban-himajin@users.noreply.github.com>
2026-02-21 18:08:20 +0900

    VBEライブラリの更新
    VBE LFBフレームバッファを扱えるようになったため少々機能を追加更新しました

--- Boot_files/32bit_Library/VBE_Operation/VBE_Operation.h
+++ Boot_files/32bit_Library/VBE_Operation/VBE_Operation.h
@@ -2,19 +2,25 @@
 #define VBE_OPERATION 1
 #define VBE_OPERATION_VERSION 1.0
 
+#pragma pack(push, 1)
 typedef struct{
-    unsigned int WIDTH;//x
-    unsigned int HEIGHT;//y
-    unsigned int one_pix_bitnum;//BitsPerPixel
-    unsigned int one_line_bytes;//BytesPerScanLine
-    unsigned int MemoryMode;//MemoryMode
+    //unsigned char WIDTH;//x
+    unsigned short WIDTH;//x
+    //unsigned char HEIGHT;//y
+    unsigned short HEIGHT;//y
+    unsigned char one_pix_bitnum;//BitsPerPixel
+    unsigned short one_line_bytes;//BytesPerScanLine
+    unsigned char MemoryMode;//MemoryMode
     unsigned int VBE_Mems;//PhyBasePtr
 }VBE_data;
+#pragma pack(pop)
+
 
 void write_pixel(const VBE_data *VBE, int x, int y, unsigned int color){
-    unsigned char* bf = *(unsigned char*)VBE->VBE_Mems;
-    unsigned int offset = (y * VBE->one_line_bytes + x * (VBE->one_pix_bitnum / 8));
-    *((unsigned int*)(bf + offset)) = color;
+    unsigned char* p = (unsigned char*)VBE->VBE_Mems + ((y * VBE->one_line_bytes) + (x * (VBE->one_pix_bitnum / 8)));
+    for(int i = 0;i < VBE->one_pix_bitnum / 8; i++){
+        p[i] = (color >> (i * 8));
+    }
     return ;
 }
 
